b6d1923302ce356f7e5dd4455def9483f654cf1f
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-06-17 20:23:44 -0700

    Use the live generic json dependency.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "/Users/josephvanboxtel/Library/CloudStorage/iCloud Drive/Documents/Programming/Projects/ServicesScheduler/ServicesScheduler/generic-json-swift")
+        .package(url: "https://github.com/javb99/generic-json-swift.git", Package.Dependency.Requirement.branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
