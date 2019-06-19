ffbc2d8c006b7f2a24fb11ba05e5354314a10cd7
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-06-19 10:56:25 -0700

    Make person relationships optional to match experienced API.

--- Sources/PlanningCenterSwift/JSON Models/Planning Center Models/Person.swift
+++ Sources/PlanningCenterSwift/JSON Models/Planning Center Models/Person.swift
@@ -15,16 +15,15 @@ public struct Person: ResourceDecodable {
     public var photoThumbnail: URL
     
     // Relationships
-    public var createdBy: ResourceIdentifier<Person>
-    public var updatedBy: ResourceIdentifier<Person>
-    
+    public var createdBy: ResourceIdentifier<Person>?
+    public var updatedBy: ResourceIdentifier<Person>?
     
     public init(resource: Resource) throws {
         id = try resource.identifer.specialize()
         fullName = try resource.attribute(for: "full_name").asString()
         photoThumbnail = try resource.attribute(for: "photo_thumbnail_url").asURL()
         // Relationships
-        createdBy = try resource.toOneRelationship(for: "created_by")
-        updatedBy = try resource.toOneRelationship(for: "updated_by")
+        createdBy = try resource.toOneRelationshipIfPresent(for: "created_by")
+        updatedBy = try resource.toOneRelationshipIfPresent(for: "updated_by")
     }
 }
