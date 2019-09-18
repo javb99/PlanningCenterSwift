aca85ea119514465847c812df1746b92ced48b71
Joe Van Boxtel <joe@pndlm.com>
2019-09-18 16:41:56 -0700

    Move the NetworkError to its own file.

--- Sources/PlanningCenterSwift/ResourceService/URLSessionService.swift
+++ Sources/PlanningCenterSwift/ResourceService/URLSessionService.swift
@@ -8,38 +8,6 @@
 import Foundation
 import JSONAPISpec
 
-public enum NetworkError: Error {
-    case noAuthorizationProvider
-    case noAuthenticationHeader   // OAuth token not available.
-    case notAuthorized            // Not authorized response from server.
-    case noHTTPResponse
-    case system(Error)
-    case decode(Error)
-    case unknown
-}
-
-extension NetworkError: CustomStringConvertible {
-    
-    public var description: String {
-        switch self {
-        case .noAuthenticationHeader:
-            return "No authenticationHeader provided."
-        case .noAuthorizationProvider:
-            return "No AuthenticationProvider supplied to URLSessionService."
-        case .notAuthorized:
-            return "Not authorized."
-        case .noHTTPResponse:
-            return "URLResponse is not an HTTPURLResposne"
-        case let .system(e):
-            return "System error: \(e)"
-        case let .decode(e):
-            return "Decode error: \(e)"
-        case .unknown:
-            return "Unkown Networking error."
-        }
-    }
-}
-
 public class URLSessionService {
     
     public let session: URLSession
