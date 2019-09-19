7405678bbd8902d9d20e6a1fe760f421bdb77912
Joe Van Boxtel <joe@pndlm.com>
2019-09-18 18:31:17 -0700

    Comment out the currently unused types.

--- Sources/PlanningCenterSwift/JSON Models/Planning Center Models/PlanTime.swift
+++ Sources/PlanningCenterSwift/JSON Models/Planning Center Models/PlanTime.swift
@@ -9,74 +9,74 @@
 //
 import Foundation
 import JSONAPISpec
-
-// MARK: - Related API
-
-public struct PlanTime {
-    
-    public static var pluralName: String = "folders"
-    
-    public var id: ResourceIdentifier<PlanTime>
-    
-    public init(id: ResourceIdentifier<PlanTime>) {
-        self.id = id
-    }
-}
-
-// MARK: - Structure
-
-extension PlanTime: ResourceProtocol {
-    
-    public enum `Type`: String, Codable {
-        case service
-        case rehearsal
-        case other
-    }
-    
-    public struct Attributes: Codable {
-        
-        enum CodingKeys: String, CodingKey {
-            case name
-            case type
-            case createdAt = "created_at"
-            case updatedAt = "updated_at"
-            case startsAt = "starts_at"
-            case endsAt = "ends_at"
-            case liveStartsAt = "live_starts_at"
-            case liveEndsAt = "live_ends_at"
-            case teamReminders = "team_reminders"
-        }
-        
-        public var name: String?
-        
-        public var type: Type
-        
-        public var createdAt: Date?
-        
-        public var updatedAt: Date?
-        
-        public var startsAt: Date
-        
-        public var endsAt: Date?
-        
-        public var liveStartsAt: Date?
-        
-        public var liveEndsAt: Date?
-        
-        /// TeamID to days notice.
-        public var teamReminders: [String: Int]?
-    }
-    
-    public struct Relationships: Codable {
-        
-//        enum CodingKeys: String, CodingKey {
-//            case assignedTeams = "assigned_teams"
-//        }
 //
-//        public var assignedTeams: ResourceIdentifier<Folder>? // To-many
-    }
-    
-    public typealias Links = Empty
-    
-    public typealias Meta = Empty
-}
+//// MARK: - Related API
+//
+//public struct PlanTime {
+//    
+//    public static var pluralName: String = "folders"
+//    
+//    public var id: ResourceIdentifier<PlanTime>
+//    
+//    public init(id: ResourceIdentifier<PlanTime>) {
+//        self.id = id
+//    }
+//}
+//
+//// MARK: - Structure
+//
+//extension PlanTime: ResourceProtocol {
+//    
+//    public enum `Type`: String, Codable {
+//        case service
+//        case rehearsal
+//        case other
+//    }
+//    
+//    public struct Attributes: Codable {
+//        
+//        enum CodingKeys: String, CodingKey {
+//            case name
+//            case type
+//            case createdAt = "created_at"
+//            case updatedAt = "updated_at"
+//            case startsAt = "starts_at"
+//            case endsAt = "ends_at"
+//            case liveStartsAt = "live_starts_at"
+//            case liveEndsAt = "live_ends_at"
+//            case teamReminders = "team_reminders"
+//        }
+//        
+//        public var name: String?
+//        
+//        public var type: Type
+//        
+//        public var createdAt: Date?
+//        
+//        public var updatedAt: Date?
+//        
+//        public var startsAt: Date
+//        
+//        public var endsAt: Date?
+//        
+//        public var liveStartsAt: Date?
+//        
+//        public var liveEndsAt: Date?
+//        
+//        /// TeamID to days notice.
+//        public var teamReminders: [String: Int]?
+//    }
+//    
+//    public struct Relationships: Codable {
+//        
+////        enum CodingKeys: String, CodingKey {
+////            case assignedTeams = "assigned_teams"
+////        }
+////
+////        public var assignedTeams: ResourceIdentifier<Folder>? // To-many
+//    }
+//    
+//    public typealias Links = Empty
+//    
+//    public typealias Meta = Empty
+//}
--- Sources/PlanningCenterSwift/JSON Models/Planning Center Models/Team.swift
+++ Sources/PlanningCenterSwift/JSON Models/Planning Center Models/Team.swift
@@ -11,104 +11,104 @@ import Foundation
 import JSONAPISpec
 
 // MARK: - Related API
-
-public struct Team{//}: CRUDable {
 //
-//    public static var pluralName: String = "teams"
+//public struct Team{//}: CRUDable {
+////
+////    public static var pluralName: String = "teams"
+////
+////    public var id: ResourceIdentifier<Team>
+////
+////    public init(id: ResourceIdentifier<Team>) {
+////        self.id = id
+////    }
+////
+////    public var subfolders: AnyCodableNetworkTransaction<Empty, ResourceCollectionDocument<Folder>> {
+////        return .init(path: instancePath + "/folders")
+////    }
+////
+////    public var serviceTypes: AnyCodableNetworkTransaction<Empty, ResourceCollectionDocument<ServiceType>> {
+////        return .init(path: instancePath + "/service_types")
+////    }
+//}
 //
-//    public var id: ResourceIdentifier<Team>
+//// MARK: - Structure
 //
-//    public init(id: ResourceIdentifier<Team>) {
-//        self.id = id
+//extension Team: ResourceProtocol {
+//
+//    fileprivate enum ScheduleTo : String, Codable {
+//        case plan
+//        case time
 //    }
 //
-//    public var subfolders: AnyCodableNetworkTransaction<Empty, ResourceCollectionDocument<Folder>> {
-//        return .init(path: instancePath + "/folders")
+//    public struct Attributes: Codable {
+//
+//        enum CodingKeys: String, CodingKey {
+//            case name
+//            case sequenceIndex = "sequence"
+//            case scheduleTo = "schedule_to"
+//            case createdAt = "created_at"
+//            case updatedAt = "updated_at"
+//            case defaultStatus = "default_status"
+//            case defaultPrepareNotifications = "default_prepare_notifications"
+//            case assignedDirectly = "assigned_directly"
+//        }
+//
+//        public var name: String?
+//
+//        public var sequenceIndex: Int?
+//
+//        fileprivate var scheduleTo: ScheduleTo
+//
+//        public var createdAt: Date?
+//
+//        public var updatedAt: Date?
+//
+//        public var defaultStatus: PositionStatus?
+//
+//        public var defaultPrepareNotifications: Bool?
+//
+//        public var assignedDirectly: Bool?
 //    }
 //
-//    public var serviceTypes: AnyCodableNetworkTransaction<Empty, ResourceCollectionDocument<ServiceType>> {
-//        return .init(path: instancePath + "/service_types")
-//    }
-}
-
-// MARK: - Structure
-
-extension Team: ResourceProtocol {
-    
-    fileprivate enum ScheduleTo : String, Codable {
-        case plan
-        case time
-    }
-    
-    public struct Attributes: Codable {
-        
-        enum CodingKeys: String, CodingKey {
-            case name
-            case sequenceIndex = "sequence"
-            case scheduleTo = "schedule_to"
-            case createdAt = "created_at"
-            case updatedAt = "updated_at"
-            case defaultStatus = "default_status"
-            case defaultPrepareNotifications = "default_prepare_notifications"
-            case assignedDirectly = "assigned_directly"
-        }
-        
-        public var name: String?
-        
-        public var sequenceIndex: Int?
-        
-        fileprivate var scheduleTo: ScheduleTo
-        
-        public var createdAt: Date?
-        
-        public var updatedAt: Date?
-        
-        public var defaultStatus: PositionStatus?
-        
-        public var defaultPrepareNotifications: Bool?
-        
-        public var assignedDirectly: Bool?
-    }
-    
-    public struct Relationships: Codable {
-        
-        enum CodingKeys: String, CodingKey {
-            case serviceType = "service_type"
-        }
-        
-        public var serviceType: ToOneRelationship<Models.ServiceType>?
-    }
-    
-    public typealias Links = Empty
-    
-    public typealias Meta = Empty
-}
-
-extension Team.Attributes {
-    
-    public var isSplit: Bool {
-        get { scheduleTo == .time }
-        set { scheduleTo = newValue ? .time : .plan }
-    }
-}
+//    public struct Relationships: Codable {
 //
-//public struct Team: ResourceDecodable {
-//    public var id: ResourceIdentifier<Team>
-//    public var name: String
-//    public var orderIndex: Int? // How the team should be ordered with other teams.
-//    public var doesScheduleToAllTimes: Bool
-//    
-//    public var serviceTypeID: ResourceIdentifier<ServiceType>
-//    //let people: [People]? // Trully optional, nil unless included.
-//    //let perPersonPositionAssignments: [PersonTeamPostionAssignment]? // Trully optional, nil unless included.
-//    //let teamPositions: [TeamPosition] // Trully optional, nil unless included.
-//    
-//    public init(resource: Resource) throws {
-//        id = try resource.identifer.specialize()
-//        
-//        name = try resource.attribute(for: "name").asString()
-//        orderIndex = try resource.attributeIfPresent(for: "sequence")?.asInt()
-//        doesScheduleToAllTimes = try resource.attribute(for: "schedule_to").asString() == "plan"
-//        serviceTypeID = try resource.toOneRelationship(for: "service_type")
+//        enum CodingKeys: String, CodingKey {
+//            case serviceType = "service_type"
+//        }
+//
+//        public var serviceType: ToOneRelationship<Models.ServiceType>?
+//    }
+//
+//    public typealias Links = Empty
+//
+//    public typealias Meta = Empty
+//}
+//
+//extension Team.Attributes {
+//
+//    public var isSplit: Bool {
+//        get { scheduleTo == .time }
+//        set { scheduleTo = newValue ? .time : .plan }
 //    }
 //}
+////
+////public struct Team: ResourceDecodable {
+////    public var id: ResourceIdentifier<Team>
+////    public var name: String
+////    public var orderIndex: Int? // How the team should be ordered with other teams.
+////    public var doesScheduleToAllTimes: Bool
+////
+////    public var serviceTypeID: ResourceIdentifier<ServiceType>
+////    //let people: [People]? // Trully optional, nil unless included.
+////    //let perPersonPositionAssignments: [PersonTeamPostionAssignment]? // Trully optional, nil unless included.
+////    //let teamPositions: [TeamPosition] // Trully optional, nil unless included.
+////
+////    public init(resource: Resource) throws {
+////        id = try resource.identifer.specialize()
+////
+////        name = try resource.attribute(for: "name").asString()
+////        orderIndex = try resource.attributeIfPresent(for: "sequence")?.asInt()
+////        doesScheduleToAllTimes = try resource.attribute(for: "schedule_to").asString() == "plan"
+////        serviceTypeID = try resource.toOneRelationship(for: "service_type")
+////    }
+////}
