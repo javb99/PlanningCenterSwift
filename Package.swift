d3ec4705ccd389515578e5ce95dbe73bb9cf0c94
Joe Van Boxtel <joe@pndlm.com>
2019-09-18 20:01:11 -0700

    Use a GitHub repository for the JSON:API spec package.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "/Users/josephvanboxtel/Documents/Programming/SwiftPackages/JSONAPI")
+        .package(url: "https://github.com/javb99/SwiftJSONAPI.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
