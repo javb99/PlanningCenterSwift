8509aa5a35d5aa4b5e7e1215632a147673142d61
Joe Van Boxtel <joe@pndlm.com>
2019-11-10 18:34:59 -0800

    Add people endpoint namespace.

---
+++ Sources/PlanningCenterSwift/Endpoints/People/PeopleEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joseph Van Boxtel on 11/10/19.
+//
+
+import Foundation
+import JSONAPISpec
+
+extension Endpoints {
+    public static var people = PeopleEndpoint()
+
+    public struct PeopleEndpoint: Endpoint {
+        
+        public typealias RequestBody = Empty
+        
+        public typealias ResponseBody = ResourceDocument<Models.Organization>
+        
+        public typealias ResourceType = Models.Organization
+        
+        public var path: Path = ["people", "v2"]
+    }
+}
