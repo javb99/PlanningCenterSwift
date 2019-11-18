58f25eb7e483dcdd59b38619103038007e44297e
Joe Van Boxtel <joe@pndlm.com>
2019-11-18 14:04:21 -0800

    Add an endpoint that can support paging.

---
+++ Sources/PlanningCenterSwift/Endpoints/Implementation/PaginatedEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Joseph Van Boxtel on 11/12/19.
+//
+
+import Foundation
+
+extension Endpoint {
+    
+    public func page(pageNumber: Int = 0, pageSize: Int = 25) -> AnyEndpoint<RequestBody, ResponseBody> {
+        let paginationParams = [URLQueryItem(name: "per_page", value: "\(pageSize)"), URLQueryItem(name: "offset", value: "\(pageNumber * pageSize)")]
+        return AnyEndpoint(method: self.method, path: self.path, queryParams: self.queryParams + paginationParams)
+    }
+    
+    public func page(offset: Int = 0, pageSize: Int = 25) -> AnyEndpoint<RequestBody, ResponseBody> {
+        let paginationParams = [URLQueryItem(name: "per_page", value: "\(pageSize)"), URLQueryItem(name: "offset", value: "\(offset)")]
+        return AnyEndpoint(method: self.method, path: self.path, queryParams: self.queryParams + paginationParams)
+    }
+}
