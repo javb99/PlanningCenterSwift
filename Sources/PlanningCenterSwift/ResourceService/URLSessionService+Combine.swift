7659f49ca1222e83f80707f777a811ac45e3f98e
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-12-07 20:04:30 -0800

    Move combine publishers to the proper file.

--- Sources/PlanningCenterSwift/ResourceService/URLSessionService+Combine.swift
+++ Sources/PlanningCenterSwift/ResourceService/URLSessionService+Combine.swift
@@ -6,70 +6,91 @@ import Foundation
 import Combine
 import JSONAPISpec
 
-
-//struct ListEndpointPublisher<Endpt: ResourceListEndpoint>: Publisher {
-//
-//    typealias Element = Resource<Endpt.InstanceEndpoint.ResourceType>
-//
-//    typealias Output = Element
-//    typealias Failure = NetworkError
-//
-//    let endpoint: Endpt
-//    let network: PCOService
-//
-//    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-//
-//        let subscription = ListSubscription(subscriber: subscriber) { (pageRange, completion)->AnyCancellable in
-//            return AnyCancellable({})
-//        }
-//        subscriber.receive(subscription: subscription)
-//    }
-//}
-
 /// Parameter: page
 typealias ResultHandler<T, E: Error> = (Result<T, E>)->()
 
-//
-//extension URLSessionService {
-//
-//    //public typealias EndpointPublisher<E: Endpoint> = AnyPublisher<(HTTPURLResponse, E, E.ResponseBody), NetworkError>
-//
-//    public func publish<Endpt: Endpoint>(_ endpoint: Endpt) -> EndpointPublisher<Endpt> where Endpt.RequestBody == JSONAPISpec.Empty {
-//        guard let request = requestBuilder.buildRequest(for: endpoint) else {
-//            return Fail(error: NetworkError.unknown).eraseToAnyPublisher()
-//        }
-//        return publish(request, for: endpoint)
-//    }
-//
-//    public func publish<Endpt: Endpoint>(_ endpoint: Endpt, body: Endpt.RequestBody) -> EndpointPublisher<Endpt> {
-//
-//        guard let request = requestBuilder.buildRequest(for: endpoint, body: body) else {
-//            return Fail(error: NetworkError.unknown).eraseToAnyPublisher()
-//        }
-//        return publish(request, for: endpoint)
-//    }
-//
-//
-//    // MARK: Implementation
-//
-//    private func publish<Endpt: Endpoint>(_ request: URLRequest, for endpoint: Endpt) -> EndpointPublisher<Endpt> {
-//
-//        return session.dataTaskPublisher(for: request)
-//            .mapError(NetworkError.system)
-//            .tryMap(errorsAre: NetworkError.self) { data, response in
-//                try self.responseHandler.handleResponse(to: endpoint, response, data: data)
-//            }
-//            .eraseToAnyPublisher()
-//    }
-//}
-//
-//extension Publisher {
-//    /// Use when the all possible thrown errors are a certain error type, E.
-//    func tryMap<E, T>(errorsAre: E.Type, transform: @escaping (Output) throws -> T) -> Publishers.MapError<Publishers.TryMap<Self, T>, E> where E: Error {
-//        return tryMap(transform).mapError(forceCast)
-//    }
-//}
-//
-//func forceCast<T>(_ value: Any) -> T {
-//    return value as! T
-//}
+public typealias PagedEndpoint<Endpt: Endpoint> = AnyEndpoint<Endpt.RequestBody, Endpt.ResponseBody>
+public typealias PagedFuture<Endpt: Endpoint> = AnyPublisher<(HTTPURLResponse, PagedEndpoint<Endpt>, Endpt.ResponseBody), NetworkError>
+public typealias EndpointFuture<Endpt: Endpoint> = AnyPublisher<(HTTPURLResponse, Endpt, Endpt.ResponseBody), NetworkError>
+
+extension URLSessionService {
+    
+    public func publisher<Endpt, R>(for endpt: Endpt, pageSize: Int = 25)
+        -> AnyPublisher<Resource<R>, NetworkError>
+        where Endpt: Endpoint, R: ResourceProtocol,
+        Endpt.ResponseBody == ResourceCollectionDocument<R>,
+        Endpt.RequestBody == JSONAPISpec.Empty {
+            
+            var totalPageCount: Int? = nil
+            func setTotalPages(_ elementCount: Int) {
+                let pageCount = elementCount.divideRoundingUp(by: pageSize)
+                totalPageCount = pageCount
+            }
+            
+            func pageExists(_ pageIndex: Int) -> Bool {
+                if pageIndex > 1, let totalPageCount = totalPageCount, pageIndex >= totalPageCount {
+                    return false
+                }
+                return true
+            }
+            
+            let pageIndexes = FunctionSequence<Int>.positiveAscending
+            return Publishers.Sequence(sequence: pageIndexes)
+                .setFailureType(to: NetworkError.self)
+                .prefix(while: pageExists)
+                .map { pageIndex in
+                    let pageEndpoint = endpt.page(pageNumber: pageIndex, pageSize: pageSize)
+                    return self.future(for: pageEndpoint)
+                }
+                .flatMap(maxPublishers: .max(1)) { (pageFuture: PagedFuture<Endpt>) in
+                    return pageFuture.map { (_, _, body: ResourceCollectionDocument<R>) -> [Resource<R>] in
+                        if let elementsCount = body.meta?.totalCount {
+                            setTotalPages(elementsCount)
+                        }
+                        return body.data ?? []
+                    }
+                }.flatten().eraseToAnyPublisher()
+    }
+    
+    public func future<Endpt>(for endpoint: Endpt) -> EndpointFuture<Endpt>
+    where Endpt: Endpoint, Endpt.RequestBody == JSONAPISpec.Empty {
+        let request = requestBuilder.buildRequest(for: endpoint)!
+        #warning("this can fail if there is not auth header")
+        return session.dataTaskPublisher(for: request)
+            .mapError(NetworkError.system)
+            .tryMap(errorsAre: NetworkError.self) { arg in
+                let (data, response) = arg
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw NetworkError.noHTTPResponse
+                }
+                let parsedData = self.responseHandler.handleResponse(to: endpoint, httpResponse, data: data, error: nil)
+                switch parsedData {
+                case let .success(httpResponse, endpoint, body):
+                    return (httpResponse, endpoint, body)
+                case let .failure(networkError):
+                    throw networkError
+                }
+                
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher where Output: Collection {
+    public func flatten() -> Publishers.FlatMap<Publishers.Sequence<Self.Output, Self.Failure>, Self> {
+        flatMap(maxPublishers: .max(1)) {
+            Publishers.Sequence<Output, Failure>(sequence: $0)
+        }
+    }
+}
+
+extension Publisher {
+    /// Use when the all possible thrown errors are a certain error type, E.
+    func tryMap<E, T>(errorsAre: E.Type, transform: @escaping (Output) throws -> T) -> Publishers.MapError<Publishers.TryMap<Self, T>, E> where E: Error {
+        return tryMap(transform).mapError(forceCast)
+    }
+}
+
+func forceCast<T>(_ value: Any) -> T {
+    return value as! T
+}
