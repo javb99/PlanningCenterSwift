ee69937f091af1feb9d8f52689c2ac9d122ecba3
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-12-07 20:04:55 -0800

    DRY up the page endpoints.

--- Sources/PlanningCenterSwift/Endpoints/Implementation/PaginatedEndpoint.swift
+++ Sources/PlanningCenterSwift/Endpoints/Implementation/PaginatedEndpoint.swift
@@ -7,12 +7,13 @@ import Foundation
 extension Endpoint {
     
     public func page(pageNumber: Int = 0, pageSize: Int = 25) -> AnyEndpoint<RequestBody, ResponseBody> {
-        let paginationParams = [URLQueryItem(name: "per_page", value: "\(pageSize)"), URLQueryItem(name: "offset", value: "\(pageNumber * pageSize)")]
-        return AnyEndpoint(method: self.method, path: self.path, queryParams: self.queryParams + paginationParams)
+        return page(offset: pageNumber * pageSize, pageSize: pageSize)
     }
     
     public func page(offset: Int = 0, pageSize: Int = 25) -> AnyEndpoint<RequestBody, ResponseBody> {
-        let paginationParams = [URLQueryItem(name: "per_page", value: "\(pageSize)"), URLQueryItem(name: "offset", value: "\(offset)")]
+        let paginationParams = [
+            URLQueryItem(name: "per_page", value: "\(pageSize)"),
+            URLQueryItem(name: "offset", value: "\(offset)")]
         return AnyEndpoint(method: self.method, path: self.path, queryParams: self.queryParams + paginationParams)
     }
 }
